c823b8a87e1fca6f33140f0e9bf2ac50d1ecaaba
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-18 14:45:44 +0800

    Create ai.names.cpp

---
+++ Plug-ins/ai.names.cpp
@@ -0,0 +1,9 @@
+#include <stream>
+int main() {
+  if new.system(user) = created: {
+    new.variable('number.name.generated'): {
+      while iostream = system; {
+        random.number.variable() }
+    new.name('X-{number.name.generated}')
+      }
+  }
